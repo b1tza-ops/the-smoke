a13be1f92a53f9412405bfe5ceb8fafd9d8809af
Paul Scripcariu <paul1mantech@gmail.com>
2026-08-24 08:26:48 +0100

    test: cover progressive gym catalogue

---
+++ tests/gameplay/test_gym_progression.py
@@ -0,0 +1,44 @@
+import unittest
+
+from game.gym import (
+    GYMS,
+    GymStatUnavailableError,
+    calculate_training_gain,
+)
+
+
+class GymProgressionTests(unittest.TestCase):
+    def test_london_progression_has_eight_gyms(self):
+        self.assertEqual(len(GYMS), 8)
+        self.assertEqual(GYMS[0].membership_cost, 0)
+        self.assertEqual(GYMS[-1].membership_cost, 10_000)
+
+    def test_gains_progress_from_starter_to_elite(self):
+        self.assertEqual(
+            calculate_training_gain(
+                "camden_community",
+                "strength",
+                10,
+            ),
+            2.0,
+        )
+        self.assertEqual(
+            calculate_training_gain(
+                "soho_london_elite",
+                "strength",
+                10,
+            ),
+            4.0,
+        )
+
+    def test_specialist_gym_rejects_unavailable_stat(self):
+        with self.assertRaises(GymStatUnavailableError):
+            calculate_training_gain(
+                "brixton_performance",
+                "dexterity",
+                10,
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
